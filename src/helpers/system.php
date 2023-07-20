0f3fec37de627f2d94f5d0a204a8ce9c3f832bc3
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2023-07-20 13:08:33 +0300

    Fixed get database error message

--- src/helpers/system.php
+++ src/helpers/system.php
@@ -81,7 +81,7 @@ function lang($key)
 function getDataError()
 {
 	if (DEBUG) {
-		$error = Database::get()->getError();
+		$error = \Core\Database::get()->getError();
 		if ($error)
 			error($error);
 	}
