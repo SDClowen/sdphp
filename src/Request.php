a1bc525735282f4b447f9c93a28f21041e5df2fb
Mahmut YILDIRIM <mahmutyildirim@outlook.com.tr>
2025-03-26 15:01:42 +0300

    Update Request.php

--- src/Request.php
+++ src/Request.php
@@ -445,40 +445,59 @@ class Request
 	 */
 	public static function filter($data = null, $filter = true)
 	{
-		$data = trim($data);
-		if (!$filter)
-			return $data;
-
-		# Fix &entity\n;
-		$data = str_replace(array('&amp;', '&lt;', '&gt;'), array('&amp;amp;', '&amp;lt;', '&amp;gt;'), $data);
-		$data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
-		$data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
-		$data = html_entity_decode($data, ENT_COMPAT, 'UTF-8');
-
-		# Remove any attribute starting with "on" or xmlns
-		$data = preg_replace('#(<[^>]+?[\x00-\x20"\'])(?:on|xmlns)[^>]*+>#iu', '$1>', $data);
-
-		# Remove javascript: and vbscript: protocols
-		$data = preg_replace('#([a-z]*)[\x00-\x20]*=[\x00-\x20]*([`\'"]*)[\x00-\x20]*j[\x00-\x20]*a[\x00-\x20]*v[\x00-\x20]*a[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2nojavascript...', $data);
-		$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*v[\x00-\x20]*b[\x00-\x20]*s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:#iu', '$1=$2novbscript...', $data);
-		$data = preg_replace('#([a-z]*)[\x00-\x20]*=([\'"]*)[\x00-\x20]*-moz-binding[\x00-\x20]*:#u', '$1=$2nomozbinding...', $data);
-
-		# Only works in IE: <span style="width: expression(alert('Ping!'));"></span>
-		$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?expression[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-		$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?behaviour[\x00-\x20]*\([^>]*+>#i', '$1>', $data);
-		$data = preg_replace('#(<[^>]+?)style[\x00-\x20]*=[\x00-\x20]*[`\'"]*.*?s[\x00-\x20]*c[\x00-\x20]*r[\x00-\x20]*i[\x00-\x20]*p[\x00-\x20]*t[\x00-\x20]*:*[^>]*+>#iu', '$1>', $data);
-
-		# Remove namespaced elements (we do not need them)
-		$data = preg_replace('#</*\w+:\w[^>]*+>#i', '', $data);
-
-		do {
-			# Remove really unwanted tags
-			$old_data = $data;
-			$data = preg_replace('#</*(?:applet|b(?:ase|gsound|link)|embed|frame(?:set)?|i(?:frame|layer)|l(?:ayer|ink)|meta|object|s(?:cript|tyle)|title|xml)[^>]*+>#i', '', $data);
-		}
-		while ($old_data !== $data);
-
-		return $data;
+	    if ($data === null || !is_string($data)) {
+	        return '';
+	    }
+	
+	    $data = trim($data);
+	    if (!$filter) {
+	        return $data;
+	    }
+	
+	    // Convert special characters to HTML entities first
+	    $data = htmlspecialchars($data, ENT_QUOTES | ENT_HTML5, 'UTF-8');
+	
+	    // Remove null bytes
+	    $data = str_replace("\0", '', $data);
+	
+	    // Remove UTF-8 characters from 0x00-0x08, 0x0B-0x0C, 0x0E-0x1F
+	    $data = preg_replace('/[\x00-\x08\x0B\x0C\x0E-\x1F\x7F]/u', '', $data);
+	
+	    // Remove potentially dangerous strings
+	    $dangerous = array(
+	        '/\\.\\.\\//', // Directory traversal
+	        '/--/',        // SQL comments
+	        '/\\<\\/?[a-z]/',  // HTML tags
+	        '/javascript:/i',   // JavaScript protocol
+	        '/vbscript:/i',    // VBScript protocol
+	        '/data:/i',        // Data protocol
+	        '/base64/i',       // Base64 encoded content
+	        '/eval\\(/i',      // eval()
+	        '/expression\\(/i', // CSS expression
+	        '/union\\s+select/i', // SQL injection
+	        '/exec\\(/i',      // Code execution
+	        '/GLOBALS/i',      // PHP globals
+	        '/_SERVER/i',      // PHP server
+	        '/_GET/i',         // PHP GET
+	        '/_POST/i',        // PHP POST
+	        '/_FILES/i',       // PHP FILES
+	        '/_COOKIE/i',      // PHP COOKIE
+	        '/_SESSION/i',     // PHP SESSION
+	        '/_ENV/i',         // PHP ENV
+	        '/_REQUEST/i',     // PHP REQUEST
+	    );
+	
+	    $data = preg_replace($dangerous, '', $data);
+	
+	    // Remove any HTML entities that might be malicious
+	    $data = preg_replace('/(&#*\w+)[\x00-\x20]+;/u', '$1;', $data);
+	    $data = preg_replace('/(&#x*[0-9A-F]+);*/iu', '$1;', $data);
+	
+	    // Remove all remaining HTML/XML tags and attributes
+	    $data = strip_tags($data);
+	
+	    // Double encode to prevent bypass attempts
+	    return htmlspecialchars($data, ENT_QUOTES | ENT_HTML5, 'UTF-8', true);
 	}
 }
 ?>
